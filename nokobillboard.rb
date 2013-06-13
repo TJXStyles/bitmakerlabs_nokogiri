ac86db810fc3a9800aba5b478a74b421b31e6f92
Bitmaker Labs <bitmakerlabs@cnc.local>
2013-06-13 18:12:47 -0400

    Formatted lyrics

--- nokobillboard.rb
+++ nokobillboard.rb
@@ -6,6 +6,7 @@ BILLBOARD = "http://www.billboard.com/charts/hot-100"
 
 song_names = []
 artist_names = []
+lyrics_array = []
 
 page = Nokogiri::HTML(open(BILLBOARD))
 original_song_names = page.css("div.listing.chart_listing h1")
@@ -28,62 +29,54 @@ original_artist_names.each { |artists| artist_names << artists.text }
 		# Grabs the link of the first search result
 		lyric_results = search_page.css(".serpresult h3 a")[0]['href']
 		lyrics_url = Nokogiri::HTML(open(lyric_results))
-		lyrics = lyrics_url.css("div#songLyricsDiv-outer p").text
-
-
+		lyrics = lyrics_url.css("div#songLyricsDiv-outer p")
+		puts "Just retrieved #{artist_names[index]} - #{song_names[index]} lyrics..."
+		lyrics_array << lyrics
 	end
 
-
 	
 
-
+File.open('chart_positions.html', 'w') do |html|
 	
 
+	for i in 1..song_names.length
+		html.write("<div class='section' id='section#{i}'>\n")
+		html.write("\t<div class='bg-opacity'></div>\n")
+		html.write("\t<div class='float'></div>\n")
+		html.write("\t<div class='container'>\n")
+		html.write("\t\t<div class='youtube'>\n")
+		html.write("\t\t\t<iframe width='498' height='280' src='http://www.youtube.com/embed/fF_sD_poO2Y' frameborder='0' allowfullscreen></iframe>\n")
+		html.write("\t\t</div>\n")
+		html.write("\t\t<div class='number'><p>\##{i}</p></div>\n")
+		html.write("\t\t<div class='song-container'>\n")
+		html.write("\t\t\t<p>\n")
+		html.write("\t\t\t\t<h1>#{song_names[i-1]}</h1>\n")
+		html.write("\t\t\t\t<h2 class='artist'>#{artist_names[i-1]}</h2>\n")
+		html.write("\t\t\t</p>\n")		
+		html.write("\t\t</div>\n")
+		html.write("\t\t<div class='lyrics'>\n")
+		html.write("\t\t\t<h3>Lyrics</h3>\n")
+		html.write("#{lyrics_array[i-1]}")
+		html.write("\t\t</div>\n")
+		html.write("\t</div>\n\n")
+		
 
 
-	
+		if i < song_names.length
+			html.write("\t\t<div class='next-song'>\n")
+			html.write("\t\t\t<a href='\#section#{i+1}'>Next song</a>\n")
+			html.write("\t\t</div>\n")
+			html.write("</div> <!-- Ending section div -->")
+		end
 
-	
-
-
-
-
-
-
-
-
+	end
+end
 
 
-# File.open('chart_positions.html', 'w') do |html|
-	
-
-# 	for i in 1..song_names.length
-# 		html.write("<div class='section' id='section#{i}'>\n")
-# 		html.write("\t<div class='bg-opacity'></div>\n")
-# 		html.write("\t<div class='float'></div>\n")
-# 		html.write("\t<div class='container'>\n")
-# 		html.write("\t\t<div class='youtube'>\n")
-# 		html.write("\t\t\t<iframe width='498' height='280' src='http://www.youtube.com/embed/fF_sD_poO2Y' frameborder='0' allowfullscreen></iframe>\n")
-# 		html.write("\t\t</div>\n")
-# 		html.write("\t\t<div class='number'><p>\##{i}</p></div>\n")
-# 		html.write("\t\t<div class='song-container'>\n")
-# 		html.write("\t\t\t<p>\n")
-# 		html.write("\t\t\t\t<h1>#{song_names[i-1]}</h1>\n")
-# 		html.write("\t\t\t\t<h2 class='artist'>#{artist_names[i-1]}</h2>\n")
-# 		html.write("\t\t\t</p>\n")		
-# 		html.write("\t\t</div>\n")
-# 		html.write("\t</div>\n\n")
 
 
-# 		if i < song_names.length
-# 			html.write("\t\t<div class='next-song'>\n")
-# 			html.write("\t\t\t<a href='\#section#{i+1}'>Next song</a>\n")
-# 			html.write("\t\t</div>\n")
-# 			html.write("</div> <!-- Ending section div -->")
-# 		end
 
-# 	end
-# end
+
 
 
 
